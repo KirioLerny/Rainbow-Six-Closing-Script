bf4569e3bf75181e132f9c1e87fc47c52ae157a3
KirioLerny <nozd-0a2f@stud.hhs.karlsruhe.de>
2024-06-29 03:31:03 +0200

    Initial Commit

---
+++ rainbowsix-deep-close.bat
@@ -0,0 +1,6 @@
+taskkill /F /IM RainbowSix_BE.exe
+taskkill /F /IM RainbowSix.exe
+taskkill /F /IM RainbowSix_Vulkan.exe
+taskkill /F /IM upc.exe
+taskkill /F /IM UplayWebCore.exe
+taskkill /F /IM UbisoftGameLauncher.exe
