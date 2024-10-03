f7cc65ef8f0d2845594e1d9653eaeeba01cb6bcf
KirioLerny <145799240+KirioLerny@users.noreply.github.com>
2024-10-03 23:41:08 +0200

    Update rainbowsix-deep-close.bat
    Updated due removal of Vulkan version of game. Changed to DX11

--- rainbowsix-deep-close.bat
+++ rainbowsix-deep-close.bat
@@ -1,6 +1,6 @@
 taskkill /F /IM RainbowSix_BE.exe
 taskkill /F /IM RainbowSix.exe
-taskkill /F /IM RainbowSix_Vulkan.exe
+taskkill /F /IM RainbowSix_DX11.exe
 taskkill /F /IM upc.exe
 taskkill /F /IM UplayWebCore.exe
 taskkill /F /IM UbisoftGameLauncher.exe
